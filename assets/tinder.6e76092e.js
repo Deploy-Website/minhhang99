d4ffbd574c27a52356175ee700c5411fd9f1dc7d
= <dongoc409@gmail.com>
2024-12-14 18:05:19 +0700

    [vinh.nt] update code

---
+++ assets/tinder.6e76092e.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{S as r}from"./vendor.adc4f508.js";import{E as e}from"./effect-tinder.922a4396.js";const o=new r(".swiper",{modules:[e],effect:"tinder",grabCursor:!0});window.swiper=o;
